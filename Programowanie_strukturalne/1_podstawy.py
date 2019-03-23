9ed073ae70a8e7c8a4877a504ee335a8b06425fd
Ignacy Nowakowski <Paderech@users.noreply.github.com>
2019-03-23 14:00:45 +0100

    Pierwszyz program

--- Programowanie_strukturalne/1_podstawy.py
+++ Programowanie_strukturalne/1_podstawy.py
@@ -8,34 +8,34 @@ print(potega)
 tekst = "CDV "
 print(tekst *2)
 
-#pobieranie danych z klawiatury
-#imie = input()
-# print("Twoje imię podane z klawiatury: "+imie)
+pobieranie danych z klawiatury
+imie = input()
+print("Twoje imię podane z klawiatury: "+imie)
 
-# nazwisko = 'Nowakowski'
-# print('Twoje dane: '+imie+' '+nazwisko)
+nazwisko = 'Nowakowski'
+print('Twoje dane: '+imie+' '+nazwisko)
 
-# print(type(imie))
-# print(type(nazwisko))
+print(type(imie))
+print(type(nazwisko))
 
-#wiek = input()
-# print('Wiek ' + wiek)
-# print(type(wiek))
+wiek = input()
+print('Wiek ' + wiek)
+print(type(wiek))
 
-# wiekAla =30
-#
-#
-# #konwerdja
-# x='5'
-# print(type(x))
-# x=int(x)
-# print(type(x))
-#
-# y=4
-# print(type(y))
-# y=4/2
-# print(type(y))
-# print(y)
+wiekAla =30
+
+
+#konwerdja
+x='5'
+print(type(x))
+x=int(x)
+print(type(x))
+
+y=4
+print(type(y))
+y=4/2
+print(type(y))
+print(y)
 
 nazwisko = 'Kowalski'
 print(nazwisko[0])
