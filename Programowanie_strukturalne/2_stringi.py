8370db6d8a06ee26fde9e67281941d4c7b32a191
Ignacy Nowakowski <Paderech@users.noreply.github.com>
2019-03-23 14:31:26 +0100

    Stringi w pythonie

--- Programowanie_strukturalne/2_stringi.py
+++ Programowanie_strukturalne/2_stringi.py
@@ -22,3 +22,35 @@ print(type(zawartosc))
 
 imie = 'Julia'
 print('\nDane użytkownika\nMasz na imię: '+imie)
+
+text1 = 'Jan\n'
+text2 = 'Nowak'
+print(text1 + text2)
+
+text1 = text1.rstrip()
+print(text1 + text2)
+print(text1 + ' ' + text2)
+
+#wyświetlanie łańcuchów znaków
+
+#wszystkie wersje pythona
+text = '%a, Java i %a' % ('PHP', "Pythine")
+print(text)
+
+#nowsze wersje pythona 2.6 i > 3
+text = '{0}, Java i {1}'.format("PHP", "Python")
+
+#help(text.teplace)
+
+new = text.replace('PHP', "C#")
+print(new)
+
+#wypisywanie danych
+rok = "2019"
+misiac = 'marzec'
+dzien = '23'
+
+# print('Data: ' + end="")
+print(dzien, miesiac, rok)
+
+# print('Data: ' + end="")
---
+++ Programowanie_strukturalne/3_losowanie.py
@@ -0,0 +1,15 @@
+#pi
+import math
+pi = math.pi
+print(pi)
+
+#pierwiastek
+pierw = math.sqrt(3)
+print(pierw)
+
+#moduł random
+import random
+losuj = random.random()
+print(losuj)
+
+#losujZlisty - random.choice
