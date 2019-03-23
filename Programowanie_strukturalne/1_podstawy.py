36da4b789718372c64b6cec02332d1871e1d36f9
Ignacy Nowakowski <Paderech@users.noreply.github.com>
2019-03-23 13:47:45 +0100

    Pierwszyz program

--- Programowanie_strukturalne/1_podstawy.py
+++ Programowanie_strukturalne/1_podstawy.py
@@ -1,2 +1,38 @@
-#pierwszy
 print("CDV")
+print(2)
+
+#potęga
+potega = 2 ** 10
+print(potega)
+
+tekst = "CDV "
+print(tekst *2)
+
+#pobieranie danych z klawiatury
+imie = input()
+print("Twoje imię podane z klawiatury: "+imie)
+
+nazwisko = 'Nowakowski'
+print('Twoje dane: '+imie+' '+nazwisko)
+
+print(type(imie))
+print(type(nazwisko))
+
+wiek = input()
+print('Wiek ' + wiek)
+print(type(wiek))
+
+wiekAla =30
+
+
+#konwerdja
+x='5'
+print(type(x))
+x=int(x)
+print(type(x))
+
+y=4
+print(type(y))
+y=4/2
+print(type(y))
+print(y)
