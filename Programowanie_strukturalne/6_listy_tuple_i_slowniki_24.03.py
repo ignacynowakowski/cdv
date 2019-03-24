484c375feda428bfa67a19716abe713d2eac4bb4
Ignacy Nowakowski <Paderech@users.noreply.github.com>
2019-03-24 15:21:29 +0100

    Tuple i Słowniki

---
+++ Programowanie_strukturalne/6_listy_tuple_i_slowniki_24.03.py
@@ -0,0 +1,13 @@
+#tuple
+imiona = ("Julia", "Anna", "Tomek")
+print(type(imiona))
+print(imiona[0])
+
+#słownik
+osoba = {"imie":"Julia", "nazwisko":"Nowak", "wiek":20}
+print(osoba)
+print(type(osoba))
+#print(osoba[0]) #błąd
+print(osoba["imie"])
+print(osoba.keys())
+print(osoba["wzrost"])
